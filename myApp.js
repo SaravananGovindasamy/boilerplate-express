f86803e29ecd1cc39e9c76bfba3ccf6707f78252
SaravananGovindasamy <161823765+SaravananGovindasamy@users.noreply.github.com>
2024-03-16 00:19:56 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,20 @@
 let express = require('express');
 let app = express();
 
+absolutePath = __dirname + '/views/index.html'
+
+console.log('Hello World');
+
+app.get('/json', (req, res) => {
+  res.json({"message": "Hello json"});
+  });
+
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(absolutePath);
+  });
+
 
 
 
