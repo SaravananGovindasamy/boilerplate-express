588cfca213ab21e9b7a6bb48f13793f705bd3992
SaravananGovindasamy <161823765+SaravananGovindasamy@users.noreply.github.com>
2024-03-18 21:38:06 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 
@@ -5,8 +7,34 @@ absolutePath = __dirname + '/views/index.html'
 
 console.log('Hello World');
 
+// Logger middleware function
+const loggerMiddleware = (req, res, next) => {
+  // Extract information from the request object
+  const method = req.method;
+  const path = req.path;
+  const ip = req.ip;
+
+  // Log the request information to the console
+  console.log(`${method} ${path} - ${ip}`);
+
+  // Call next() to move to the next middleware or route handler
+  next();
+};
+
+// Mount the logger middleware to be executed for all requests
+app.use(loggerMiddleware);
+
 app.get('/json', (req, res) => {
   res.json({"message": "Hello json"});
+});
+
+/*
+app.get('/json', (req, res) => {
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE  === 'uppercase') {
+      message = message.toUpperCase();
+    }
+    res.json({"message": message});
   });
 
 app.use('/public', express.static(__dirname + '/public'))
@@ -14,40 +42,4 @@ app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
     res.sendFile(absolutePath);
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+*/
